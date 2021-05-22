a67647882bd17e44d95d07332d7b5599ffc519dd
Lori-Ann Williams <lori-ann.williams@hotmail.com>
2021-05-22 13:52:28 -0400

    run socketio-flask webserver. emit message. handle message

--- src/app.py
+++ src/app.py
@@ -1,12 +1,32 @@
-from flask import Flask, redirect
+from flask import Flask, render_template, redirect
 import random
 import string
+from flask_socketio import SocketIO, send
+
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'secret!'
+
+socketio = SocketIO(app, cors_allowed_origins="*")
+
+@socketio.on("message")
+def handleMessage(msg):
+    a = 1/0
+    print(msg)
+    send(msg, broadcast=True)
+    return None
 
 @app.route('/')
 def main():
     # Home page
     return """
+<script src="https://cdnjs.cloudflare.com/ajax/libs/socket.io/4.0.1/socket.io.js" integrity="sha512-q/dWJ3kcmjBLU4Qc47E4A9kTB4m3wuTY7vkFJDTZKjTs8jhyGQnaUrxa0Ytd0ssMZhbNua9hE+E7Qv1j+DyZwA==" crossorigin="anonymous"></script>
+<script type="text/javascript" charset="utf-8">
+    var socket = io();
+    socket.on('connect', function() {
+        socket.emit('message', {data: "I'm connected!"});
+    });
+</script>
 <form action="/game" method="post">
   <input type="submit" value="New Game">
 </form>
@@ -36,3 +56,6 @@ Press start when ready
 def game(game_id):
     return f"You are now playing game: {game_id}"
 
+
+if __name__ == '__main__':
+    socketio.run(app, host='0.0.0.0', port=8080, use_reloader=True)
